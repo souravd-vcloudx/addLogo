90c50a6c750ca17e0d19f9574a94f022cd59efad
souravd-vcloudx <87314199+souravd-vcloudx@users.noreply.github.com>
2022-06-07 23:41:17 +0530

    Create addLogo.js

---
+++ addLogo.js
@@ -0,0 +1,5 @@
+function enxHookRoomConnected() {
+    var img = document.createElement('img');
+    img.src = 'https://web.enablex.io/img/customers/logo-1564699880.png';
+    document.querySelector('.navbar').prepend(img);
+}
