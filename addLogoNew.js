0c992b935748fab7bfa0345594c608da8429d357
souravd-vcloudx <87314199+souravd-vcloudx@users.noreply.github.com>
2022-06-08 17:45:39 +0530

    Update and rename addLogo.js to addLogoNew.js

--- addLogoNew.js
+++ addLogoNew.js
@@ -1,5 +1,5 @@
 function enxHookRoomConnected() {
     var img = document.createElement('img');
     img.src = 'https://web.enablex.io/img/customers/logo-1564699880.png';
-    document.querySelector('.navbar').prepend(img);
+    document.querySelector('.navbar').children[0].prepend(img);
 }
